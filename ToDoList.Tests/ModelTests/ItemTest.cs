1e18c9b782dad49782a8068453d644b2d4435b00
Brit Wallace <britwallace888@gmail.com>
2022-02-05 17:13:54 -0800

    Add tests
    Co-Authored-by: Chris Nakayama <cnakayam@gmail.com>

--- ToDoList.Tests/ModelTests/ItemTest.cs
+++ ToDoList.Tests/ModelTests/ItemTest.cs
@@ -1,13 +1,50 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
+using System.Collections.Generic;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTests
+  public class ItemTests : IDisposable
   {
 
-    // Test methods will go here.
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+// Arrange
+    List<Item> newList = new List<Item> { };
+
+// Act
+    List<Item> result = Item.GetAll();
+
+// Assert
+    CollectionAssert.AreEqual(newList, result);
+    }
 
   }
 }
