5be9127f2d44022a3ba54f1a532b3c772185e93e
Brit Wallace <britwallace888@gmail.com>
2022-02-27 11:55:02 -0800

    Add RESTful routing & objects within objects refactor

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -33,12 +33,24 @@ namespace ToDoList.Controllers
       Item.ClearAll();
       return View();
     }
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+  public ActionResult Show(int categoryId, int itemId)
+  {
+    Item item = Item.Find(itemId);
+    Category category = Category.Find(categoryId);
+    Dictionary<string, object> model = new Dictionary<string, object>();
+    model.Add("item", item);
+    model.Add("category", category);
+    return View(model);
+  }
+  [HttpGet("/categories/{categoryId}/items/new")]
+  public ActionResult New(int categoryId)
+  {
+    Category category = Category.Find(categoryId);
+    return View(category);
+  }
 
-    [HttpGet("/items/{id}")]
-    public ActionResult Show(int id)
-    {
-      Item foundItem = Item.Find(id);
-      return View(foundItem);
-    }
+
+    
   }
 }
