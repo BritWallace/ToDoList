6c253f0708d241b2d73988b2449ab2e44291203b
Brit Wallace <britwallace888@gmail.com>
2022-03-05 20:30:10 -0800

    Add Delete and Edit files to Items folder

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
@@ -22,51 +23,49 @@ namespace ToDoList.Controllers
 
     public ActionResult Create()
     {
-        return View();
+      return View();
     }
 
     [HttpPost]
     public ActionResult Create(Item item)
     {
-        _db.Items.Add(item);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
+      _db.Items.Add(item);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
+
     public ActionResult Details(int id)
-{
-    Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-    return View(thisItem);
-}
+    {
+      Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
+    }
+    public ActionResult Edit(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Item item)
+    {
+      _db.Entry(item).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      _db.Items.Remove(thisItem);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
-// namespace ToDoList.Controllers
-// {
-//   public class ItemsController : Controller
-//   {
-
-//     [HttpGet("/categories/{categoryId}/items/new")]
-//     public ActionResult New(int categoryId)
-//     {
-//       Category category = Category.Find(categoryId);
-//       return View(category);
-//     }
-
-//     [HttpPost("/items/delete")]
-//     public ActionResult DeleteAll()
-//     {
-//       Item.ClearAll();
-//       return View();
-//     }
-
-//     [HttpGet("/categories/{categoryId}/items/{itemId}")]
-//     public ActionResult Show(int categoryId, int itemId)
-//     {
-//       Item item = Item.Find(itemId);
-//       Category category = Category.Find(categoryId);
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       model.Add("item", item);
-//       model.Add("category", category);
-//       return View(model);
-//     }
-//   }
-// }
